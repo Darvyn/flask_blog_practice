62b9631c79feb1d04e230a396427c5ec69393fda
Darvyn <Xavier.ytx@gmail.com>
2018-03-25 01:05:23 +0400

    add the delete statement of SQLite

--- connect.py
+++ connect.py
@@ -2,9 +2,14 @@ import sqlite3
 conn = sqlite3.connect('blog.db')
 cur = conn.cursor()
 
+query_del = '''delete from blogpost where id=4;'''
+cur.execute(query_del)
+
 query = '''select * from blogpost;'''
 cur.execute(query)
 rows = cur.fetchall()
 
-for i in rows:
-    print(type(i))
+for row in rows:
+  print(row)
+
+
